b2797380f3688d369516e7a64d5fa339ffd90236
md0501 <michaelhuntsman01@gmail.com>
2024-02-20 10:25:45 -0600

    part 1

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data(){
+        return {
+        product: 'Socks!',
+        description: 'Blue or Green'
+        }
+    }
+})
+//<!--Creates vue instance and opp -->  
+//Connects option object (product)
+//we connect product to the app in the <h1> {{product}} in the html files
+//whenebr wee change the product value, reactivity occurs and vue updates
+
+//
